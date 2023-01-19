8285d8d64d5d5b5229be29e40e71bc23179fcba4
wooju <dnwnrnt@naver.com>
2023-01-19 18:23:58 +0900

    Initial Commit

---
+++ MyAsset1/MyAsset1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyAsset1App.swift
+//  MyAsset1
+//
+//  Created by wooju on 2023/01/19.
+//
+
+import SwiftUI
+
+@main
+struct MyAsset1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
