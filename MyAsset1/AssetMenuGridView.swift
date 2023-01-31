172ee84203a4621c179c8401925ce98d29a6fc6b
wooju <dnwnrnt@naver.com>
2023-02-01 00:29:02 +0900

    button style

---
+++ MyAsset1/AssetMenuGridView.swift
@@ -0,0 +1,35 @@
+//
+//  AssetMenuGridView.swift
+//  MyAsset1
+//
+//  Created by wooju on 2023/01/31.
+//
+
+import SwiftUI
+
+struct AssetMenuGridView: View {
+    let menuList: [[AssetMenu]] = [
+        [.creditScore, .bankAccount, .investment, .loan],
+        [.insurance, .creditCard, .cash, .realEstate]
+    ]
+    var body: some View {
+        VStack(spacing: 20) {
+            ForEach(menuList, id: \.self) { row in
+                HStack(spacing: 10){
+                    ForEach(row) { menu in
+                        Button(" "){
+                            print("\(menu.title)버튼 tapped")
+                        }
+                        .buttonStyle(AssetMenuButtonStyle(menu: menu))
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AssetMenuGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetMenuGridView()
+    }
+}
